24b6e107463eacd5d25796a0f53efcd417adfb25
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-19 00:44:25 -0500

    Bootstrap styling applied to Topbar

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 export default class Topbar extends Component {
     render() {
         return (
-            <div>Topbar</div>
+            <div>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">
+                                SIRHAMY
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
