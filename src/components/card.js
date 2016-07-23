0b2df69c884e6b2db18fa00ced75e529c9c5e701
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-23 18:59:44 -0500

    Created cards and basic square shapes

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class card extends Component {
     render() {
         return (
-            <div>This is a card</div>
+            <div className={ `card-grid-card ${this.props.details.type == 'code' ? 'red-card' : 'blue-card' } `}>
+                <strong>{this.props.details.title}</strong>
+                <div>Check if this stretches</div>
+            </div>
         );
     }
 }
--- src/components/card_holder.js
+++ src/components/card_holder.js
@@ -5,14 +5,28 @@ import Card from './card';
 var ProjectData = require('../../resources/data/projects.json');
 
 export default class CardHolder extends Component {
+    
+    renderCards() {
+        return ProjectData.projects.map((project) => {
+            console.log("Rendering card");
+            return (
+                    <Card key={project.name} details={project}/>
+            );
+        });
+    }
+    
     render() {
         //Check if json data loaded
         console.log(ProjectData);
+        
         return (
             <div>
-              This is a card holder.
-              <Card />
+                This is a card holder
+                <div className="card-grid">
+                    {this.renderCards()}
+                </div>
             </div>
         );
+        
     }
 }
