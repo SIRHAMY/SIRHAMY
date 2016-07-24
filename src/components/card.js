028a80ba407b2ec37c42839bbdb2d776983fc3f1
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-23 20:27:17 -0500

    Cards are mostly square

--- src/components/card.js
+++ src/components/card.js
@@ -4,8 +4,10 @@ export default class card extends Component {
     render() {
         return (
             <div className={ `card-grid-card ${this.props.details.type == 'code' ? 'red-card' : 'blue-card' } `}>
-                <strong>{this.props.details.title}</strong>
-                <div>Check if this stretches</div>
+                <div className="card-content">
+                    <strong>{this.props.details.title}</strong>
+                    <div>Check if this stretches</div>
+                </div>
             </div>
         );
     }
