c56e9e3d41e0eda23d95f4ee011e46ba911696f1
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-30 11:50:37 -0500

    Cards are now black

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class card extends Component {
     render() {
         return (
-            <div className={ `card-grid-card ${this.props.details.type == 'code' ? 'red-card' : 'blue-card' } `}>
+            <div className={ `card-grid-card black-card `}>
                 <div className="card-content">
                     <strong>{this.props.details.title}</strong>
                     <div>Check if this stretches</div>
