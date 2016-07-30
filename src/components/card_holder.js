7e5f2666d5edf822b2303a3907bebe7f36de426e
Hamilton Greene <hamilton.greene@gmail.com>
2016-07-30 16:46:15 -0500

    Built base functions to begin coupling with Medium integration

--- src/components/card_holder.js
+++ src/components/card_holder.js
@@ -5,10 +5,58 @@ import Card from './card';
 var ProjectData = require('../../resources/data/projects.json');
 
 export default class CardHolder extends Component {
+
+
+    componentWillMount() {
+        this.cards = {};
+
+        var mediumIn = false;
+        var codeIn = false;
+        var designIn = false;
+        
+        switch(this.props.currPage) {
+            case "home":
+                mediumIn = true;
+                codeIn = true;
+                designIn = true;
+                break;
+            case "projects":
+                codeIn = true;
+                designIn = true;
+                break;
+        }
+
+        //Ingest data
+
+        var toIngest = {
+            "medium": mediumIn,
+            "code": codeIn,
+            "design": designIn
+        }
+
+        this.ingestData(toIngest);
+
+    }
+
+    ingestData(toIngest) {
+        var tempCards = [];
+        if(toIngest.medium) {
+            //Ingest medium posts
+        }
+
+        if(toIngest.code) {
+            //Ingest code posts
+            //var toAdd = ProjectData.projects.map();
+            tempCards = [...tempCards, ...ProjectData.projects];
+        }
+
+        this.setState( { cards: tempCards } );
+    }
     
     renderCards() {
-        return ProjectData.projects.map((project) => {
+        return this.state.cards.map((project) => {
             console.log("Rendering card");
+            //console.log(project);
             return (
                     <Card key={project.name} details={project}/>
             );
