f182e834fd8d6301189a645064956403142d3d27
Hamilton Greene <hamilton.greene@gmail.com>
2016-09-16 00:14:39 -0400

    Feature: Created card underlayer as foundation for inserting project pictures

--- src/components/card.js
+++ src/components/card.js
@@ -24,18 +24,33 @@ export default class card extends Component {
         return returnClass;
     }
 
+    getBackgroundImage() {
+        if(this.props.details.image != null) {
+            return { 
+                backgroundImage: 'url(/resources/images/card_backgrounds/' + this.props.details.image + ')',
+                backgroundSize: '100% 100%'
+            }
+        } else {
+            return { 
+                backgroundImage: 'url(/resources/icons/' + this.props.details.type + '-icon.png)',
+                backgroundSize: '100% 100%'
+             }
+        }
+    }
+
     render() {
         return (
             <HamLink link={this.props.details.link}>
-                <div>
-                    <div className="card-grid-card black-card">
+                <div className="card-grid-card">
+                    <div className="card-grid-content black-card">
                         <div className="card-content">
                             <h4>{this.props.details.title}</h4>
                         </div>
                         <div className={`project-type-display ${this.getCardTypeIcon()}`}>
                         </div>
                     </div>
-                    
+                    <div className="card-grid-content-hidden" style={this.getBackgroundImage()}>
+                    </div>
                 </div>
             </HamLink>
         );
