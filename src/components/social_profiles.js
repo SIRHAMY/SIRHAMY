6d50ad79cbdb96266568037a2c7392d4caacc6f2
Hamilton Greene <hamilton.greene@gmail.com>
2016-09-18 02:09:51 -0400

    Feature: Began creating social connect elements

--- src/components/social_profiles.js
+++ src/components/social_profiles.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
+import SocialProfile from './social_profile';
+
 export default class SocialProfiles extends Component {
-    
+    render() {
+        return (
+            <div className="social-profile-holder">
+
+                <SocialProfile image="LinkedIn" toLink="http://linkedin.com"/>
+
+                <SocialProfile image="LinkedIn" toLink="http://linkedin.com"/>
+                
+            </div>
+        );
+    }
 }
